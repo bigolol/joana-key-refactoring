3ca43e01eefb1a7a3a6d32966a92017beb2857b2
Holger Klein <holgerhugoklein@gmail.com>
2017-07-18 14:40:17 +0200

    asd

--- proofObs/proofs/jzip/MyZipInputStream.java
+++ proofObs/proofs/jzip/MyZipInputStream.java
@@ -1,14 +1,13 @@
 package jzip;
 public class MyZipInputStream{
 private byte[] zipFile;
-	public MyZipInputStream(byte[] zipFile) {
-		this.zipFile = zipFile;
-	}
+	public MyZipInputStream(byte[] zipFile) {
+		this.zipFile = zipFile;
+	}
 
-	/*@ requires true;
-	  @ determines \result \by zipFile; */
-public byte[] read() {
-		return zipFile;
-	}
+	public byte[] read() {
+		return zipFile;
+	}
 
 }
+
